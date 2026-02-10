6199587f2de9d3d264b2b0a30a7362d084b1f340
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-02-10 21:08:35 +0530

    Finalize API boundary with POST /predict endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from app.schemas import HouseInput
+from app.services import predict_house_price
+
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Backend service is up and running!"}
+
+@app.post("/predict")
+def predict_house(data: HouseInput):
+    price = predict_house_price(data)
+    return {
+        "predicted_price": price,
+        "currency": "INR"
+        }
