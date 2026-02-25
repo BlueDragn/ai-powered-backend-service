fb97a8bd24ff69c41c0dd52679bbf4fdc8c8a670
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-02-26 00:35:53 +0530

    feat(flow-a):Load california housing dataset and verify shapes

--- training/train.py
+++ training/train.py
@@ -1,9 +1,15 @@
 """
-Flow A - Training Pipeline California Housing Dataset
+Flow A - Training Pipeline
+Dataset: California Housing
 """
-
+from sklearn.datasets import fetch_california_housing
 def main():
-    print("Flow A initialization.")
+    print("Loading California Housing dataset... ")
+    data = fetch_california_housing()
+
+    print("Dataset loaded successfully.")
+    print("Feature shape:", data.data.shape)
+    print("Target shape", data.target.shape)
 
 if __name__ == "__main__":
     main()
